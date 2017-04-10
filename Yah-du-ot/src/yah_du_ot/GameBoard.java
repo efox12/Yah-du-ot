f3e191ed91b21ea00accb44548f1720ee5178f14
max mckee <max mckee>
2017-04-10 14:36:32 -0700

    Changed GameBoard

--- Yah-du-ot/src/yah_du_ot/GameBoard.java
+++ Yah-du-ot/src/yah_du_ot/GameBoard.java
@@ -4,4 +4,10 @@ public class GameBoard {
 	private Grouping[] rows;
 	private Grouping[] columns;
 	private Grouping[] clusters;
+	
+	public GameBoard() {
+		rows = new Grouping[9];
+		columns = new Grouping[9];
+		clusters = new Grouping[9];
+	}
 }
