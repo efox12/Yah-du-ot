d358dda8f34bbebe30c5b98c0482785f3958a885
max mckee <max mckee>
2017-04-11 21:51:58 -0700

    Added UI elements

--- Yah-du-ot/src/yah_du_ot/YahDuOt.java
+++ Yah-du-ot/src/yah_du_ot/YahDuOt.java
@@ -1,6 +1,9 @@
 package yah_du_ot;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,8 +13,8 @@ public class YahDuOt {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() ->
         {
-        	Menu mainMenu = new Menu();
         	YahduotUX game = new YahduotUX();
+        	Menu mainMenu = new Menu(game);
         	mainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         	game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         	mainMenu.setVisible(true);
@@ -22,9 +25,14 @@ public class YahDuOt {
 
 class Menu extends JFrame {
 	
-	
 	public Menu(YahduotUX game) {
+		this.setSize(new Dimension(500, 500));
+		
 		JButton newGame = new JButton("New Game");
+		newGame.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 40));
+		newGame.addActionListener(event -> game.setVisible(true));
+		newGame.addActionListener(event -> this.setVisible(false));
+		getContentPane().add(newGame, BorderLayout.CENTER);
 		
 	}
 }
