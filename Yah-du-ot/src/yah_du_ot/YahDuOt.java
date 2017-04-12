f7d60a5257a76bdb97cfb2e98657d66b4107015b
max mckee <max mckee>
2017-04-12 11:46:48 -0700

    Modidified UX elements

--- Yah-du-ot/src/yah_du_ot/YahDuOt.java
+++ Yah-du-ot/src/yah_du_ot/YahDuOt.java
@@ -4,7 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Toolkit;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -24,12 +26,19 @@ public class YahDuOt {
 }
 
 class Menu extends JFrame {
+	private static Toolkit kit = Toolkit.getDefaultToolkit();
+	private static Dimension screen = kit.getScreenSize();
+	private static final int MAX_HEIGHT = (int) (0.4 * screen.height);
+	private static final int MAX_WIDTH = (int) (0.4 * screen.width);
+	private static final Font STANDARD_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 40);
 	
 	public Menu(YahduotUX game) {
-		this.setSize(new Dimension(500, 500));
+		this.setSize(new Dimension(MAX_WIDTH, MAX_WIDTH));
+		this.setLocation(((screen.width - MAX_WIDTH) / 2), ((screen.height - MAX_WIDTH) / 2));
 		
+		//this.setLayout(new BoxLayout());
 		JButton newGame = new JButton("New Game");
-		newGame.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 40));
+		newGame.setFont(STANDARD_FONT);
 		newGame.addActionListener(event -> game.setVisible(true));
 		newGame.addActionListener(event -> this.setVisible(false));
 		getContentPane().add(newGame, BorderLayout.CENTER);
