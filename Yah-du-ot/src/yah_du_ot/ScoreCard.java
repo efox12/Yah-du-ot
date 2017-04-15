0cb932a99a6d04d41087f38e1c9f8066718d3a48
Will Fraisl <willfraisl@Wills-MacBook-Pro.local>
2017-04-14 17:54:55 -0700

    Created ScoreCard class

---
+++ Yah-du-ot/src/yah_du_ot/ScoreCard.java
@@ -0,0 +1,17 @@
+package yah_du_ot;
+
+import javax.swing.JFrame;
+
+public class ScoreCard {
+	private int grandTotal;
+	private int[] lineTallies;
+	
+	public ScoreCard(String playerName){
+		grandTotal = 0;
+		
+	}
+	
+	public JFrame displayCard(){
+		
+	}
+}
