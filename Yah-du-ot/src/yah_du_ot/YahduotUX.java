bc448fe0f05d25428db34cf19a5312bdb7a0389f
Max McKee <ibathow368@live.com>
2017-04-21 11:52:17 -0700

    Added to UI
    Added buttons and row/column labels. Framework for drawing lines
    established. Created a box around the buttons to appear like board.

--- Yah-du-ot/src/yah_du_ot/YahduotUX.java
+++ Yah-du-ot/src/yah_du_ot/YahduotUX.java
@@ -1,19 +1,25 @@
 package yah_du_ot;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,6 +29,7 @@ public class YahduotUX extends JFrame{
 	private static final int GAME_HEIGHT = (int) (0.75 * kit.getScreenSize().height);
 	private static final int GAME_WIDTH = (int) (0.75 * kit.getScreenSize().width);
 	private final Font BOARD_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, GAME_WIDTH / 30);
+	private static Drawing myDrawing;
 	
 	public YahduotUX() {
 		this.setTitle("Yah-du-ot");
@@ -98,24 +105,35 @@ public class YahduotUX extends JFrame{
 		
 		c.fill = GridBagConstraints.BOTH;
 		c.gridx = 2;
-		c.gridy = 0;
+		c.gridy = 1;
+		c.gridx = 2;
 		c.gridheight = 1;
 		c.weighty = 0.1;
 		c.weightx = 0.1;
 		playerInfo.validate();
 		getContentPane().add(playerInfo, c);
 		
-		c.gridy = 1;
+		c.gridy = 2;
 		getContentPane().add(roll, c);
 		
-		c.gridx = 0;
 		c.gridy = 0;
+		c.gridx = 1;
+		c.weighty = 0;
+		getContentPane().add(createColumnLabels(), c);
+		
+		c.gridy = 1;
+		c.gridx = 0;
+		c.gridheight = 2;
+		getContentPane().add(createRowLabels(), c);
+		
+		c.gridy = 1;
+		c.gridx = 1;
 		c.gridwidth = 0;
 		c.gridheight = 2;
-		c.gridwidth = 2;
-		c.weightx = 0.3;
+		c.gridwidth = 1;
+		c.weightx = 0.9;
 		c.weighty = 1.0;
-		getContentPane().add(filler, c);
+		getContentPane().add(createButtonMap(), c);
 		
 		
 		/*
@@ -124,7 +142,81 @@ public class YahduotUX extends JFrame{
     	mainMenu.setVisible(true);
     	*/
     	this.setVisible(true);
+    	myDrawing = new Drawing();
+    	setGlassPane(myDrawing);
+    	myDrawing.setOpaque(false);
+    	myDrawing.setVisible(true);
 	}	
+	
+	private Container createColumnLabels() {
+		Container columns = new Container();
+		columns.setLayout(new GridLayout(1, 9));
+		
+		JLabel temp = new JLabel();
+		for(char s = 'A'; s <= 'I'; s++) {
+			temp = new JLabel(Character.toString(s));
+			temp.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, GAME_HEIGHT / 30));
+			temp.setHorizontalAlignment(JLabel.CENTER);
+			temp.setVerticalAlignment(JLabel.CENTER);
+			columns.add(temp);
+		}
+		
+		return columns;
+	}
+	
+	private Container createRowLabels() {
+		Container rows = new Container();
+		rows.setLayout(new GridLayout(9, 1));
+		
+		JLabel temp = new JLabel();
+		for(int i = 1; i <= 9; i++) {
+			temp = new JLabel(Integer.toString(i));
+			temp.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, GAME_HEIGHT / 30));
+			temp.setHorizontalAlignment(JLabel.CENTER);
+			temp.setVerticalAlignment(JLabel.CENTER);
+			rows.add(temp);
+		}
+		
+		return rows;
+	}
+	
+	private Container createButtonMap() {
+		Container buttons = new Container();
+		buttons.setLayout(new GridLayout(9, 9));
+		JButton [][] buttonBox = new JButton[9][9];
+		
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				String label = Character.toString((char) (i + 65)) + (j + 1) + "";
+				buttonBox[i][j] = new JButton(label);
+				buttonBox[i][j].setFont(BOARD_FONT);
+				buttonBox[i][j].setForeground(Color.RED);
+				buttonBox[i][j].addActionListener(event ->
+													System.out.println(label));
+				buttons.add(buttonBox[i][j]);
+			}
+		}
+		
+		return buttons;
+	}
+	
+	class Drawing extends JPanel {
+		
+		public Drawing() {
+			this.setBounds(0, 0, GAME_WIDTH, GAME_HEIGHT);
+			this.setPreferredSize(super.getPreferredSize());
+		}
+		
+		protected void paintComponent(Graphics g) {
+			Graphics2D g2 = (Graphics2D) g;
+			Rectangle2D box = new Rectangle2D.Double(GAME_WIDTH * 0.02395, GAME_HEIGHT * 0.04784, GAME_WIDTH * 0.647, GAME_HEIGHT * 0.91);
+			g2.setStroke(new BasicStroke(5));
+			//g2.fillRect(10, 10, 100, 100);
+			g2.draw(box);
+		}
+		
+		
+	}
 	
 	class Menu extends JFrame {
 		private final Dimension screen = kit.getScreenSize();
@@ -191,6 +283,6 @@ public class YahduotUX extends JFrame{
 			super.paintComponent(g);
 		    g.drawImage(bg, -70, -100, this);
 		  }
-		}
+	}
 }
 
