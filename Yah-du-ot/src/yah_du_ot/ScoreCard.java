7650fa391c05513582c5907e7a623b15b0ae3173
Will Fraisl <willfraisl@Wills-MacBook-Pro.local>
2017-04-21 11:54:07 -0700

    Scorecard done
    (Hopefully)

--- Yah-du-ot/src/yah_du_ot/ScoreCard.java
+++ Yah-du-ot/src/yah_du_ot/ScoreCard.java
@@ -6,6 +6,7 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
 
 public class ScoreCard {
 	private int grandTotal;
@@ -32,8 +33,23 @@ public class ScoreCard {
 	}
 	
 	public JFrame displayCard(){
-		//Object[][] tableData = new Object[students.keySet().size()][numberOfColumns];
-		ScoreCardFrame scoreFrame = new ScoreCardFrame();
+		//if this won't display the enums, then change so it is strings
+		Object[][] tableData = {
+				{Line.FullHouse,"25 Points", lineTallies.get(Line.FullHouse)},
+				{Line.ThreeOK, "10 Points", lineTallies.get(Line.ThreeOK)},
+				{Line.FourOK, "20 Points", lineTallies.get(Line.FourOK)},
+				{Line.FiveOK, "30 Points", lineTallies.get(Line.FiveOK)},
+				{Line.SixOK, "40 Points", lineTallies.get(Line.SixOK)},
+				{Line.SevenOK, "50 Points", lineTallies.get(Line.SevenOK)},
+				{Line.EightOK, "60 Points", lineTallies.get(Line.EightOK)},
+				{Line.NineOK, "100 Points", lineTallies.get(Line.NineOK)},
+				{Line.SStraight, "20 Points", lineTallies.get(Line.SStraight)},
+				{Line.LStraight, "30 Points", lineTallies.get(Line.LStraight)},
+				{Line.FStraight, "40 Points", lineTallies.get(Line.FStraight)},
+		};
+		String[] columnNames = {"Type", "Points worth", "Times scored"};
+		JTable table = new JTable(tableData, columnNames);
+		ScoreCardFrame scoreFrame = new ScoreCardFrame(table);
 		scoreFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		scoreFrame.setVisible(true);
 		return scoreFrame;
@@ -41,8 +57,9 @@ public class ScoreCard {
 	
 	class ScoreCardFrame extends JFrame{
 		
-		public ScoreCardFrame(){
+		public ScoreCardFrame(JTable table){
 			this.setTitle("ScoreCard");
+			this.add(table);
 		}
 	}
 }
