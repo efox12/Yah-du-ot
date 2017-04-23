65c877c610d1cabd9914c83e4d721bf17e84fd1b
Will Fraisl <willfraisl@147.222.215.193>
2017-04-23 00:16:38 -0700

    More scorecard work

--- Yah-du-ot/src/yah_du_ot/ScoreCard.java
+++ Yah-du-ot/src/yah_du_ot/ScoreCard.java
@@ -2,18 +2,22 @@ package yah_du_ot;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Toolkit;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 public class ScoreCard {
 	private int grandTotal;
 	private Dictionary<Line, Integer> lineTallies;
+	private String player;
 	
 	public ScoreCard(String playerName){
 		grandTotal = 0;
+		player = playerName;
 		lineTallies = new Hashtable<Line, Integer>();
 		lineTallies.put(Line.FullHouse, 0);
 		lineTallies.put(Line.ThreeOK, 0);
@@ -33,30 +37,50 @@ public class ScoreCard {
 	}
 	
 	public JFrame displayCard(){
+		grandTotal = lineTallies.get(Line.FullHouse)*25 +
+				lineTallies.get(Line.ThreeOK)*10 +
+				lineTallies.get(Line.FourOK)*20 +
+				lineTallies.get(Line.FiveOK)*30 +
+				lineTallies.get(Line.SixOK)*40 +
+				lineTallies.get(Line.SevenOK)*50 +
+				lineTallies.get(Line.EightOK)*60 +
+				lineTallies.get(Line.NineOK)*100 +
+				lineTallies.get(Line.SStraight)*20 +
+				lineTallies.get(Line.LStraight)*30 +
+				lineTallies.get(Line.FStraight)*40;
 		Object[][] tableData = {
-				{"FullHouse","25 Points", lineTallies.get(Line.FullHouse)},
-				{"ThreeOK", "10 Points", lineTallies.get(Line.ThreeOK)},
-				{"FourOK", "20 Points", lineTallies.get(Line.FourOK)},
-				{"FiveOK", "30 Points", lineTallies.get(Line.FiveOK)},
-				{"SixOK", "40 Points", lineTallies.get(Line.SixOK)},
-				{"SevenOK", "50 Points", lineTallies.get(Line.SevenOK)},
-				{"EightOK", "60 Points", lineTallies.get(Line.EightOK)},
-				{"NineOK", "100 Points", lineTallies.get(Line.NineOK)},
-				{"SStraight", "20 Points", lineTallies.get(Line.SStraight)},
-				{"LStraight", "30 Points", lineTallies.get(Line.LStraight)},
-				{"FStraight", "40 Points", lineTallies.get(Line.FStraight)},
+				{"Type", "Points worth", "Times Scored", "Score"},
+				{"Full House","25 Points", lineTallies.get(Line.FullHouse), lineTallies.get(Line.FullHouse)*25},
+				{"Three of a Kind", "10 Points", lineTallies.get(Line.ThreeOK), lineTallies.get(Line.ThreeOK)*10},
+				{"Four of a Kind", "20 Points", lineTallies.get(Line.FourOK), lineTallies.get(Line.FourOK)*20},
+				{"Five of a Kind", "30 Points", lineTallies.get(Line.FiveOK), lineTallies.get(Line.FiveOK)*30},
+				{"Six of a Kind", "40 Points", lineTallies.get(Line.SixOK), lineTallies.get(Line.SixOK)*40},
+				{"Seven of a Kind", "50 Points", lineTallies.get(Line.SevenOK), lineTallies.get(Line.SevenOK)*50},
+				{"Eight of a Kind", "60 Points", lineTallies.get(Line.EightOK), lineTallies.get(Line.EightOK)*60},
+				{"Nine of a Kind", "100 Points", lineTallies.get(Line.NineOK), lineTallies.get(Line.NineOK)*100},
+				{"Small Straight", "20 Points", lineTallies.get(Line.SStraight), lineTallies.get(Line.SStraight)*20},
+				{"Large Straight", "30 Points", lineTallies.get(Line.LStraight), lineTallies.get(Line.LStraight)*30},
+				{"Full Straight", "40 Points", lineTallies.get(Line.FStraight), lineTallies.get(Line.FStraight)*40},
+				{"Grand Total", "-", "-", grandTotal} 
 		};
-		String[] columnNames = {"Type", "Points worth", "Times scored"};
+		String[] columnNames = {"Type", "Points worth", "Times scored", "Score"};
 		JTable table = new JTable(tableData, columnNames);
+		JScrollPane scrollPane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
 		ScoreCardFrame scoreFrame = new ScoreCardFrame(table);
 		return scoreFrame;
 	}
 	
 	class ScoreCardFrame extends JFrame{
-		
 		public ScoreCardFrame(JTable table){
-			setTitle("ScoreCard");
+			final Toolkit kit = Toolkit.getDefaultToolkit();
+			final int GAME_HEIGHT = (int) (0.75 * kit.getScreenSize().height);
+			final int GAME_WIDTH = (int) (0.75 * kit.getScreenSize().width);
+			setSize(new Dimension(GAME_WIDTH / 2, GAME_HEIGHT / 2));
+			setLocation(GAME_WIDTH / 3, GAME_HEIGHT / 3);
+			setTitle(player + "'s ScoreCard");
 			add(table);
+			setResizable(false);
 			setVisible(true);
 		}
 	}
